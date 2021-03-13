d7ef313e15e79fc1212ee5b3db899a22fc293c49
Allison Branch <aab4ad@virginia.edu>
2021-03-13 13:51:16 -0500

    Added movie carousel
    Fixed some random stuff
    Found a plugin to use for displaying list of movies/shows called ow.carousel that I think should work pretty well
    Started working on a home page
    Added separate file for main header

---
+++ browse.php
@@ -0,0 +1,175 @@
+<!-- http:localhost/project/index.html -->
+
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+      <link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
+      <link rel="stylesheet" href="owlcarousel/assets/owl.theme.default.min.css">
+      <link rel="stylesheet" href="styles.css">
+
+    </head>
+
+    <body>
+
+    <?php include('header.html') ?>
+
+    <div class="main-page-area">
+
+      <!-- Set up your HTML -->
+      <h1 class="page-title"> My Lists </h1>
+
+
+      <h2 class="list-title"> List 1 </h2>
+      <div class="owl-carousel owl-theme">
+          <div class="item">
+              <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVpWLG11PojFPeqpJyc14l1iTPUtb-7K9nONUphboJDuTEUdXQNfPgHzcttWQhMCSvsS6SSo5qmheyWPwmLPGmqpfCGXV8MvxrJMeLUbMgxLFAOgxWcw8on2bnW3.jpg?r=748" ></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT4b-L4lOWAUoOrJmd9FvafStZx30Du2K8JS5D9UDoR86h6rbcYGLwKXPax41P5WYyW28Rs4hH6-iTnpRr4sHSIudUk.webp?r=7cf"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZBFSa5ZzaSLKxdpxm9cfl_1B1xy3tNFjbjcChJFWzNRJUJYbtEKYjVD4vEDzM798SJEflDk_ui0wA5KSnFtK7fTo_A.webp?r=db3"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABesAM_VziyoP7w00s73XNNOUEvGHIOQ-v_WzXBYXRQjHUSwGLWVxNngldaUq94TOTnrRKnyntj42VXhA-RrYCkqOKAJoFkPz3479seUKUQXaNraxQMoeu7nvqpgS.jpg?r=2f9"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABchn_R-T4AbrP8GTDKKgsUjM0Qc8eeZVyJD3vr2PYlfp-ezPxkYw74GIxvZQE3WWuZjjkniA8_OxsSAe-UjV_VAGU9M.webp?r=c59"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbfe8xIEa_hk0scnpRGwojPmP1ihGEsXJhnK43myciQkOgnZtf2CK2TZc12W3h4_2MzSBQU_tegt30jOdAFMw4iHtKE.webp?r=447"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeKsCNeMcaFlDSqW-9iM-iXAWZ4EJbaiU-xB26TSU8jLQ4wtqD8wdb7bfVIdZd49dMYZLeWAe4FOxEOgaGXxRxvbr7o.webp?r=346"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS5KksxMnJrkw_dcJypT_UhPFzelf3xlXykdh8sV_IzEeE7SOJjB4TXhb_-nhmW4vql4p2jDa--NZk__UGCtl4aol1Aa-7jp2XC8SaRqS7mkmZPNsrm0O7G0av8b.jpg?r=4d2"></img>
+          </div>
+      </div>
+
+      <h2 class="list-title"> List 2 </h2>
+      <div class="owl-carousel owl-theme">
+          <div class="item">
+              <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVpWLG11PojFPeqpJyc14l1iTPUtb-7K9nONUphboJDuTEUdXQNfPgHzcttWQhMCSvsS6SSo5qmheyWPwmLPGmqpfCGXV8MvxrJMeLUbMgxLFAOgxWcw8on2bnW3.jpg?r=748" ></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT4b-L4lOWAUoOrJmd9FvafStZx30Du2K8JS5D9UDoR86h6rbcYGLwKXPax41P5WYyW28Rs4hH6-iTnpRr4sHSIudUk.webp?r=7cf"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZBFSa5ZzaSLKxdpxm9cfl_1B1xy3tNFjbjcChJFWzNRJUJYbtEKYjVD4vEDzM798SJEflDk_ui0wA5KSnFtK7fTo_A.webp?r=db3"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABesAM_VziyoP7w00s73XNNOUEvGHIOQ-v_WzXBYXRQjHUSwGLWVxNngldaUq94TOTnrRKnyntj42VXhA-RrYCkqOKAJoFkPz3479seUKUQXaNraxQMoeu7nvqpgS.jpg?r=2f9"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABchn_R-T4AbrP8GTDKKgsUjM0Qc8eeZVyJD3vr2PYlfp-ezPxkYw74GIxvZQE3WWuZjjkniA8_OxsSAe-UjV_VAGU9M.webp?r=c59"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbfe8xIEa_hk0scnpRGwojPmP1ihGEsXJhnK43myciQkOgnZtf2CK2TZc12W3h4_2MzSBQU_tegt30jOdAFMw4iHtKE.webp?r=447"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeKsCNeMcaFlDSqW-9iM-iXAWZ4EJbaiU-xB26TSU8jLQ4wtqD8wdb7bfVIdZd49dMYZLeWAe4FOxEOgaGXxRxvbr7o.webp?r=346"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS5KksxMnJrkw_dcJypT_UhPFzelf3xlXykdh8sV_IzEeE7SOJjB4TXhb_-nhmW4vql4p2jDa--NZk__UGCtl4aol1Aa-7jp2XC8SaRqS7mkmZPNsrm0O7G0av8b.jpg?r=4d2"></img>
+          </div>
+      </div>
+
+      <h2 class="list-title"> List 3 </h2>
+      <div class="owl-carousel owl-theme">
+          <div class="item">
+              <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVpWLG11PojFPeqpJyc14l1iTPUtb-7K9nONUphboJDuTEUdXQNfPgHzcttWQhMCSvsS6SSo5qmheyWPwmLPGmqpfCGXV8MvxrJMeLUbMgxLFAOgxWcw8on2bnW3.jpg?r=748" ></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT4b-L4lOWAUoOrJmd9FvafStZx30Du2K8JS5D9UDoR86h6rbcYGLwKXPax41P5WYyW28Rs4hH6-iTnpRr4sHSIudUk.webp?r=7cf"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZBFSa5ZzaSLKxdpxm9cfl_1B1xy3tNFjbjcChJFWzNRJUJYbtEKYjVD4vEDzM798SJEflDk_ui0wA5KSnFtK7fTo_A.webp?r=db3"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABesAM_VziyoP7w00s73XNNOUEvGHIOQ-v_WzXBYXRQjHUSwGLWVxNngldaUq94TOTnrRKnyntj42VXhA-RrYCkqOKAJoFkPz3479seUKUQXaNraxQMoeu7nvqpgS.jpg?r=2f9"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABchn_R-T4AbrP8GTDKKgsUjM0Qc8eeZVyJD3vr2PYlfp-ezPxkYw74GIxvZQE3WWuZjjkniA8_OxsSAe-UjV_VAGU9M.webp?r=c59"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbfe8xIEa_hk0scnpRGwojPmP1ihGEsXJhnK43myciQkOgnZtf2CK2TZc12W3h4_2MzSBQU_tegt30jOdAFMw4iHtKE.webp?r=447"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeKsCNeMcaFlDSqW-9iM-iXAWZ4EJbaiU-xB26TSU8jLQ4wtqD8wdb7bfVIdZd49dMYZLeWAe4FOxEOgaGXxRxvbr7o.webp?r=346"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS5KksxMnJrkw_dcJypT_UhPFzelf3xlXykdh8sV_IzEeE7SOJjB4TXhb_-nhmW4vql4p2jDa--NZk__UGCtl4aol1Aa-7jp2XC8SaRqS7mkmZPNsrm0O7G0av8b.jpg?r=4d2"></img>
+          </div>
+      </div>
+
+      <h2 class="list-title"> List 4 </h2>
+      <div class="owl-carousel owl-theme">
+          <div class="item">
+              <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVpWLG11PojFPeqpJyc14l1iTPUtb-7K9nONUphboJDuTEUdXQNfPgHzcttWQhMCSvsS6SSo5qmheyWPwmLPGmqpfCGXV8MvxrJMeLUbMgxLFAOgxWcw8on2bnW3.jpg?r=748" ></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT4b-L4lOWAUoOrJmd9FvafStZx30Du2K8JS5D9UDoR86h6rbcYGLwKXPax41P5WYyW28Rs4hH6-iTnpRr4sHSIudUk.webp?r=7cf"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZBFSa5ZzaSLKxdpxm9cfl_1B1xy3tNFjbjcChJFWzNRJUJYbtEKYjVD4vEDzM798SJEflDk_ui0wA5KSnFtK7fTo_A.webp?r=db3"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABesAM_VziyoP7w00s73XNNOUEvGHIOQ-v_WzXBYXRQjHUSwGLWVxNngldaUq94TOTnrRKnyntj42VXhA-RrYCkqOKAJoFkPz3479seUKUQXaNraxQMoeu7nvqpgS.jpg?r=2f9"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABchn_R-T4AbrP8GTDKKgsUjM0Qc8eeZVyJD3vr2PYlfp-ezPxkYw74GIxvZQE3WWuZjjkniA8_OxsSAe-UjV_VAGU9M.webp?r=c59"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbfe8xIEa_hk0scnpRGwojPmP1ihGEsXJhnK43myciQkOgnZtf2CK2TZc12W3h4_2MzSBQU_tegt30jOdAFMw4iHtKE.webp?r=447"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeKsCNeMcaFlDSqW-9iM-iXAWZ4EJbaiU-xB26TSU8jLQ4wtqD8wdb7bfVIdZd49dMYZLeWAe4FOxEOgaGXxRxvbr7o.webp?r=346"></img>
+          </div>
+          <div class="item">
+            <img src="https://occ-0-2433-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS5KksxMnJrkw_dcJypT_UhPFzelf3xlXykdh8sV_IzEeE7SOJjB4TXhb_-nhmW4vql4p2jDa--NZk__UGCtl4aol1Aa-7jp2XC8SaRqS7mkmZPNsrm0O7G0av8b.jpg?r=4d2"></img>
+          </div>
+      </div>
+
+    </div>
+
+
+  </body>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="owlcarousel/owl.carousel.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      autoHeight:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          400:{
+              items:2
+          },
+          600:{
+              items:3
+          },
+          800:{
+              items:4
+          },
+          1000:{
+              items:5
+          },
+          1200:{
+              items:6
+          }
+      }
+    })
+</script>
+
+</html>
